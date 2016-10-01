d8fabb1a81c03df58564eb7fb84dc81bde71e4ec
crazy8597 <crazy8597@hotmail.com>
2016-10-01 17:07:14 +0900

    Programming assignment 2
    done with it yeah!

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+this function will make cached matrix for inverse matrix and make those things as list.
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- matrix()
+  set <- function(y){
+    x <<- y
+    inverse <<- matrix()
+  }
+  get <- function() x
+  setinverse <- function(x) inverse <<- solve(x)
+  getinverse <- function() inverse
+  list(set= set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
 ## Write a short comment describing this function
-
+this function will calculate if it does not have inverse matrix yet. 
 cacheSolve <- function(x, ...) {
+  inverse <- x$getinverse()
+  if(!is.na(inverse)){
+    message("Get inverse matrix")
+    return(inverse)
+  }
+  matrix_data <- x$get()
+  x$setinverse(matrix_data)
+  x$getinverse()
         ## Return a matrix that is the inverse of 'x'
 }
